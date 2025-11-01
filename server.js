7a80054bab80862e5f574bf4d4f7b72b38a4f65f
ziydulloislomzoda833-oss <ziydulloislomzoda833@gmail.com>
2025-11-01 10:59:14 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Aviro Social is running 🚀");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
